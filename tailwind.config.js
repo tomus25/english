4958da43d5d0e70104b7ee29285db8401a7108a7
tomus25 <tomus25@icloud.com>
2025-10-29 11:23:27 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: { extend: {} },
-  plugins: [require("@tailwindcss/typography")], // см. пункт ниже
+  plugins: [],
 };
