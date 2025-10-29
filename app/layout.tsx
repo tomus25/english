3f931ae44f598036c6ee77fb766364a062587e0f
tomus25 <tomus25@icloud.com>
2025-10-29 11:06:39 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,15 @@
+// app/layout.tsx
+import "./globals.css";
+
 export const metadata = {
   title: "FindYourTeacher",
-  description: "Найди своего идеального преподавателя английского"
+  description: "Найди своего идеального преподавателя английского",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ru">
-      <head />
-      <body>{children}</body>
+      <body className="bg-white text-slate-900">{children}</body>
     </html>
   );
 }
