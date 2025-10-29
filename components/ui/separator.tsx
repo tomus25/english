7b0108f4a64ce7499030a6109799641cb77a4683
tomus25 <tomus25@icloud.com>
2025-10-29 10:29:58 -0700

    Update separator.tsx

--- components/ui/separator.tsx
+++ components/ui/separator.tsx
@@ -1,1 +1,5 @@
+import * as React from "react";
 
+export function Separator({ className = "", ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  return <div className={`h-px w-full bg-slate-200 ${className}`} {...props} />;
+}
